c46167341a6e66c10256dbf37167543f9d54d980
Qursch <66182320+Qursch@users.noreply.github.com>
2021-02-15 15:04:47 -0500

    Updated Close and Removed Bumping
    Changed splitting raw by space to checking if the first 13 characters match.
    Removed bumping warning; will implement a daily loop checking for topics to close in the future.

--- plugin.rb
+++ plugin.rb
@@ -95,46 +95,13 @@ after_initialize do
         if post.post_number != 1 && post.user_id != -1 then
 
             # Close Topic Command        
-            raw = post.raw.downcase.split  
-            if raw[0] == "@system" then
-                if raw[1] == "close" then
-                    if post.user.primary_group_id != nil then
-                        group = Group.find_by(id: post.user.primary_group_id)
-                        if group.name == "Helpers" then
-                            topic = Topic.find_by(id: post.topic_id)
-                            topic.update_status("closed", true, bot)
-                        end
-                    end
-                end
-            else
-                # Bumping Old Topics
-                topic = Topic.find_by(id: post.topic_id)
-                category = Category.find_by(id: topic.category_id)
-                if category.auto_close_hours != nil  && category.auto_close_hours != 0 then
-                    lPC = Post.find_by(topic_id: topic.id, post_number: topic.posts_count-1).created_at.to_s
-                    last_post_time = DateTime.new(lPC[0,4].to_i, lPC[5,2].to_i, lPC[8,2].to_i).to_time.to_i
-                    sC = post.created_at.to_s
-                    post_time = DateTime.new(sC[0,4].to_i, sC[5,2].to_i, sC[8,2].to_i).to_time.to_i
-                    closetime = category.auto_close_hours * 3600
-
-                    if post_time - last_post_time > closetime then
-                        post = PostCreator.create(bot,
-                                    skip_validations: true,
-                                    topic_id: topic.id,
-                                    raw: "Hello @" + post.user.username + ", it appears that this topic should be closed due to it's category's automatic timer. If this is the case, your reply is considered to be bumping this topic, which is against the rules."
-                                )
-                        unless post.nil?
-                            post.save(validate: false)
-                        end
-                    elsif post_time - last_post_time > closetime - 172800 then
-                        post = PostCreator.create(bot,
-                                    skip_validations: true,
-                                    topic_id: topic.id,
-                                    raw: "Hello @" + post.user.username + ", it appears that this topic should be closing in less than 48 hours due to it's category's automatic timer. If this is the case, your reply will keep this topic open for another " + (category.auto_close_hours/24).to_s + " days."
-                                )
-                        unless post.nil?
-                            post.save(validate: false)
-                        end
+            raw = post.raw.downcase
+            if raw[0, 13] == "@system close" then
+                if post.user.primary_group_id != nil then
+                    group = Group.find_by(id: post.user.primary_group_id)
+                    if group.name == "Helpers" then
+                        topic = Topic.find_by(id: post.topic_id)
+                        topic.update_status("closed", true, bot)
                     end
                 end
             end
